e6927a4352eccbf2a81dae75f49d4f925b2fc024
Matheus Vinicius <devmatheusoxs@gmail.com>
2024-05-30 12:56:16 -0300

    Controllers and Views

---
+++ RootsBurguer/Controllers/LancheController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RootsBurguer.Migrations;
+using RootsBurguer.Repositories.Interfaces;
+using RootsBurguer.ViewModels;
+
+namespace RootsBurguer.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            // ViewData["Titulo"] = "Todos os Lanches";
+            // ViewData["Data"] = DateTime.Now;
+
+            // var totalLanches = lanches.Count();
+            // ViewBag.Total = "Total de Lanches: ";
+            // ViewBag.TotalLanches = totalLanches;
+
+            //----------------------------------------------------------------------//
+
+            // var lanches = _lancheRepository.Lanches;
+            // return View(lanches);
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancheRepository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheListViewModel);
+        }
+    }
+}
---
+++ RootsBurguer/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using RootsBurguer.Models;
+
+namespace RootsBurguer.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
